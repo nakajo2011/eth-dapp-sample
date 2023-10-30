47ad6986f752e66ea5fbb07d35e7221b1f7871e3
Yukishige Nakajo <nakajo@chaintope.com>
2023-10-30 11:31:59 +0900

    Implemented MyToken by according to the instructions.

---
+++ packages/contracts/contracts/MyToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply);
+    }
+}
