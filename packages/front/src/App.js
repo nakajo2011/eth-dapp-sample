5a9621be4587d253c7aae9c9018ead725833cfca
Yukishige Nakajo <nakajo@chaintope.com>
2023-11-02 15:20:35 +0900

    Implemented SPA and metamask.

--- packages/front/src/App.js
+++ packages/front/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MetaMaskProvider } from '@metamask/sdk-react';
 import Menu from './components/Menu';
 import Home from './components/Home';
 
 function App() {
     return (
-        <Router>
-            <Menu />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
+        <MetaMaskProvider>
+            <Router>
+                <Menu />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
+        </MetaMaskProvider>
     );
 }
 
--- packages/front/src/components/Home.js
+++ packages/front/src/components/Home.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useSDK } from '@metamask/sdk-react';
+import { Button, Card, CardContent, Typography } from '@mui/material';
+import BigNumber from "bignumber.js";
 
 function Home() {
+    const { sdk, connected, connecting, provider, chainId, account, balance } = useSDK();
+
+    const connect = async () => {
+        if (!connected && !connecting && sdk) {
+            await sdk.connect();
+        }
+    };
+
+    const weiToEth = (wei) => {
+        const weiBN = new BigNumber(wei, 16);  // Convert hex string to BN instance
+        const divisor = new BigNumber(10).pow(new BigNumber(18));  // 1 Ether = 10^18 Wei
+        return weiBN.div(divisor).decimalPlaces(5);  // Convert Wei to Ether
+    };
+
+    const formattedBalance = balance ? weiToEth(balance) : null;
+
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div style={{ padding: '20px' }}>
+            <Button variant="contained" color="primary" onClick={connect} disabled={connected || connecting}>
+                Connect to MetaMask
+            </Button>
+            {connected && account && (
+                <Card style={{ marginTop: '20px' }}>
+                    <CardContent>
+                        <Typography variant="h5">Account Details</Typography>
+                        <Typography variant="body1">Address: {account}</Typography>
+                        <Typography variant="body1">Balance: {formattedBalance ? `${formattedBalance} ETH` : 'Loading...'}</Typography>
+                    </CardContent>
+                </Card>
+            )}
         </div>
     );
 }
