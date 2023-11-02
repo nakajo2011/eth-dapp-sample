0bb7256a9d0ef7319843a4ae35f23731ebe7dd95
Yukishige Nakajo <nakajo@chaintope.com>
2023-11-02 15:45:35 +0900

    Implemented SPA and metamask.

--- packages/front/src/components/Home.js
+++ packages/front/src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSDK } from '@metamask/sdk-react';
 import { Button, Card, CardContent, Typography } from '@mui/material';
-import BigNumber from "bignumber.js";
+import BigNumber from 'bignumber.js';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { IconButton } from '@mui/material';
 
 function Home() {
     const { sdk, connected, connecting, provider, chainId, account, balance } = useSDK();
@@ -13,12 +15,16 @@ function Home() {
     };
 
     const weiToEth = (wei) => {
-        const weiBN = new BigNumber(wei, 16);  // Convert hex string to BN instance
+        const weiBN = new BigNumber(wei.substring(2), 16);  // Convert hex string to BN instance
         const divisor = new BigNumber(10).pow(new BigNumber(18));  // 1 Ether = 10^18 Wei
         return weiBN.div(divisor).decimalPlaces(5);  // Convert Wei to Ether
     };
 
     const formattedBalance = balance ? weiToEth(balance) : null;
+
+    const copyAddressToClipboard = () => {
+        navigator.clipboard.writeText(account);
+    };
 
     return (
         <div style={{ padding: '20px' }}>
@@ -26,13 +32,20 @@ function Home() {
                 Connect to MetaMask
             </Button>
             {connected && account && (
-                <Card style={{ marginTop: '20px' }}>
-                    <CardContent>
-                        <Typography variant="h5">Account Details</Typography>
-                        <Typography variant="body1">Address: {account}</Typography>
-                        <Typography variant="body1">Balance: {formattedBalance ? `${formattedBalance} ETH` : 'Loading...'}</Typography>
-                    </CardContent>
-                </Card>
+                <div>
+                    <Card style={{ marginTop: '20px' }}>
+                        <CardContent>
+                            <Typography variant="h5">Account Details</Typography>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <Typography variant="body1">Address: {account}</Typography>
+                                <IconButton onClick={copyAddressToClipboard}>
+                                    <ContentCopyIcon />
+                                </IconButton>
+                            </div>
+                            <Typography variant="body1">Balance: {formattedBalance ? `${formattedBalance} ETH` : 'Loading...'}</Typography>
+                        </CardContent>
+                    </Card>
+                </div>
             )}
         </div>
     );
